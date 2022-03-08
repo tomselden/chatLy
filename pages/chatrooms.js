fed3fb4a53900a2221c571b52c71cfaa3a0394ef
milcab <milcarodriguez23@gmail.com>
2022-03-07 22:38:31 -0500

    add a placeholder for the group Items

--- pages/chatrooms.js
+++ pages/chatrooms.js
@@ -1,14 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Col, Container, Row } from 'react-bootstrap'
-import Avatar from '../components/Avatar'
-import styles from '../styles/Home.module.css'
+import { Col, Container, Row, Stack } from 'react-bootstrap'
+import GroupItem from '../components/GroupItem/GroupItem.component'
+
+const fakeGroups = [
+    { groupName: 'cats rule', lastMessage: 'meow', when: '2mins ago', unReadMessages: 5 },
+    { groupName: 'cats rule twice', lastMessage: 'meow ++', when: '5mins ago', unReadMessages: 35 },
+    { groupName: 'cats rule forever', lastMessage: 'pr pr pr pr', when: '5mins ago', unReadMessages: 7 },
+]
 
 export default function Chatrooms() {
     return (
         <Container fluid>
             <Row className='full-height'>
-                <Col style={{ border: '1px solid black' }} xs={3}>list of chats</Col>
+                <Col style={{ border: '1px solid black' }} xs={3}>
+                    <Stack gap={3}>
+                        {fakeGroups.map(groupData => <GroupItem {...groupData} />)}
+                    </Stack>
+
+
+                </Col>
                 <Col style={{ border: '1px solid black' }} xs={9}>open chat</Col>
             </Row>
         </Container>
