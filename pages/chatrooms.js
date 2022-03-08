aeca665199e5853d1feb5cc19ff2b89e49cb0a95
milcab <milcarodriguez23@gmail.com>
2022-03-07 22:00:02 -0500

    create two columns layout

---
+++ pages/chatrooms.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Col, Container, Row } from 'react-bootstrap'
+import Avatar from '../components/Avatar'
+import styles from '../styles/Home.module.css'
+
+export default function Chatrooms() {
+    return (
+        <Container fluid>
+            <Row className='full-height'>
+                <Col style={{ border: '1px solid black' }} xs={3}>list of chats</Col>
+                <Col style={{ border: '1px solid black' }} xs={9}>open chat</Col>
+
+            </Row>
+
+        </Container>
+    )
+}
