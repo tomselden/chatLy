be27e2e9c62b986feb2e1d8763089f4fde5d5c57
milcab <milcarodriguez23@gmail.com>
2022-03-16 14:16:16 -0400

    css tweaks for the sign page

---
+++ components/AppLogo/AppLogo.js
@@ -0,0 +1,5 @@
+import Image from 'next/image'
+
+export default function AvatarLogo() {
+    return <img src="/logo.png" alt="me" width="400" height="400" objectPosition="center" />
+}
--- components/AvatarLogo.js
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-function AvatarLogo() {
-  return <Image src="/logo.png" alt="me" width="400" height="400" objectPosition="center"/>
-}
-
-export default AvatarLogo
